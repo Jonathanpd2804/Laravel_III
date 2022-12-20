1815b2fb9189fcb41c2c46c11081209134247680
Jonathanpd2804 <jonathanpd280403@gmail.com>
2022-12-20 10:32:17 +0000

    Ejercicio 4

---
+++ resources/views/layouts/presupuesto.blade.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>DAVID</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <!-- bootstrap 4 -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <!-- jquery  -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <!-- popper  -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <!-- bootstrap 4 js  -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <!-- sweetalert  -->     
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <style>
+
+            img{
+                float:top;
+                width:180px;
+                margin-left:70px;
+            }
+            
+            #datos{
+                float:left;
+                margin-top:180px;
+                line-height : 5px;
+            }
+
+            #cliente{
+                float:right;
+                padding-top:20px;
+                width:300px;
+                height: 100px;
+                border-style: solid;
+            }
+            #cliente p{
+                text-align:center;
+            }
+
+            #presupuesto{
+                float:right;
+                margin-right:-200px;
+                margin-top:300px;
+                border-style: solid;
+                padding-right:75px;
+                padding-left:75px;
+            }
+
+            #datospresupuesto{
+                float:right;
+                margin-top:360px;
+                margin-right:-335px;
+
+            }
+
+            #datos1presupuesto{ /*numero*/
+                float:right;
+                border-style: solid;
+                width:115px;
+                margin-right:135px;
+                text-align:center;
+
+            }
+            #datos11presupuesto{ /*numero2*/
+                float:right;
+                border-style: solid;
+                width:115px;
+                margin-top:30px;
+                margin-right:135px;
+                text-align:center;
+
+            }
+
+            #datos2presupuesto{ /*fecha2*/
+                float:left;
+                border-style: solid;
+                width:115px;
+                margin-right:373px;
+                text-align:center;
+
+
+            }
+
+            #datos22presupuesto{ /*fecha2*/
+                float:left;
+                border-style: solid;
+                width:115px;
+                margin-top:30px;
+                margin-right:135px;
+                text-align:center;
+
+            }
+
+            #informacionpresupuesto{
+                margin-top: 440px;
+                height:25px;
+                border-style: solid;
+            }
+
+            #informacionpresupuesto div{
+                float:left;
+            }
+
+            #descripcion{
+                text-align:center;
+                border-right: solid;
+                width:345px;
+            }
+
+            #unidad{
+                text-align:center;
+                border-right: solid;
+                width:120px;
+            }
+            #cantidad{
+                text-align:center;
+                border-right: solid;
+                width:115px;
+            }
+            #importe{
+                text-align:center;
+                width:113px;
+            }
+
+            #contenido{
+                margin-top:20px;
+                height:350px;
+                border-style: solid;
+                padding:10px;
+
+            }
+
+            #totales{
+                margin-top:20px;
+                height:92px;
+                width:228px;
+                border-style: solid;
+                float:right;
+            }
+
+            #subtotal{
+                text-align:center;
+                border-right: solid;
+                border-bottom: solid;
+                width:115px;
+                height:30px;
+            }
+
+            #igic{
+                text-align:center;
+                border-right: solid;
+                width:115px;
+                height:30px;
+            }
+
+            #total{
+                text-align:center;
+                border-right: solid;
+                border-top: solid;
+                width:115px;
+                height:30px;
+            }
+            #subtotalr{
+                float:right;
+                text-align:center;
+                border-bottom: solid;
+                width:113px;
+                height:30px;
+                margin-top:-94px
+            }
+
+            #igicr{
+                float:right;
+                text-align:center;
+                width:113px;
+                height:30px;
+                margin-top:-63px
+            }
+
+            #totalr{
+                float:right;
+                text-align:center;
+                border-top: solid;
+                width:113px;
+                height:30px;
+                margin-top:-32px
+            }
+
+            #pago{
+                line-height : 15px;
+                margin-top:59px;
+                float:left;
+            }
+
+            #pequeno{
+                font-size:12px;
+            }
+
+            #espaciado{
+                margin-left:145px;
+            }
+            
+            #objetodescripcion{
+                width: 345px;
+            }
+            #objetoprecio{
+                float:right;
+                margin-top:-25px;
+                margin-right:220px;
+                text-align:center;
+                width: 120px;
+            }
+            #objetocantidad{
+                float:right;
+                margin-top:-25px;
+                margin-right:105px;
+                text-align:center;
+                width: 115px;
+            }
+            #objetototal{
+                float:right;
+                margin-top:-25px;
+                text-align:center;
+                width: 113px;
+            }
+                       
+        </style>
+
+
+    </head>
+    <body>
+        <div class="panel">
+            @yield('contenido')
+        </div>
+
+
+    </body>
+</html>
--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -1,4 +1,6 @@
+@extends('layouts.app')
 
+@section("contenido")
 
     <h1>Listado de precios</h1>
     
@@ -25,8 +27,74 @@
         </table>
     </div>
 
+<script>
+     $(document).ready(function() {
+    $('#tabla').dataTable( {
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        },
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
+     $(document).ready(function(){
+
+        
 
 
+$("#tabla").on("click",".bi-trash-fill",function(e){
+    e.preventDefault();
+    //confirmar con sweetalert
+    Swal.fire({
+        title: '¿Está seguro?',
+        text: "No podras revertir esta accion",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, borrar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            //redireccionar a la url
+            //borrar con ajax
+            const tr=$(this).closest("tr"); //tr más cercano al botón, o sea el tr que contiene al botón
+            const id=tr.data("id"); //obtener el id del tr
+            $.ajax({
+                url: "{{url('/precios')}}/"+id,
+                method: "DELETE",
+                data: {
+                    "_token": "{{ csrf_token() }}"
+                },
+                success: function(){
+                    tr.fadeOut();
+                }
+            })
+        }
+    })    
+});
+});
+
+$(document).ready( function () {
+    $('#tabla').DataTable({
+        "columnDefs": [
+            
+          
+          { "orderable": false, "targets": 1 },//ocultar para columna 1
+          { "orderable": false, "targets": 2 },//ocultar para columna 1
+
+          //`Asi para cada columna`...
+        ],
+    });
+} );
+
+
+</script>
+
+    
+
+@endsection
 
 
 
